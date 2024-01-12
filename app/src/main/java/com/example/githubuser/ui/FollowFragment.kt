8fce4611504c1a748f1d35f10cefbc07c7b92b3d
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-01-12 14:50:54 +0700

    add follow fragment

---
+++ app/src/main/java/com/example/githubuser/ui/FollowFragment.kt
@@ -0,0 +1,42 @@
+package com.example.githubuser.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.githubuser.R
+import com.example.githubuser.adapter.FollowPagerAdapter
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FollowFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FollowFragment : Fragment() {
+
+    companion object {
+        const val ARG_SECTION_NUMBER = "section_number"
+        const val GITHUB_USERNAME = "github_username"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_follow, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val username = arguments?.getString(GITHUB_USERNAME)
+    }
+
+}
