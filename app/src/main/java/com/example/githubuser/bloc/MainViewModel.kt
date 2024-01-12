2e93478e5c521a70558ee717154c67256c316be9
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-01-12 08:24:45 +0700

    add maun view module

--- app/src/main/java/com/example/githubuser/bloc/MainViewModel.kt
+++ app/src/main/java/com/example/githubuser/bloc/MainViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.githubuser.bloc
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.githubuser.data.response.GithubUsersResponse
 
 class MainViewModel : ViewModel() {
+
     
 }
