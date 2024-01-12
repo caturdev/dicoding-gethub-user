630608facc3b38c17c1632411cbbe9bff3c00c83
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-01-12 15:50:29 +0700

    update api service get followers and following

--- app/src/main/java/com/example/githubuser/data/retrofit/ApiSevice.kt
+++ app/src/main/java/com/example/githubuser/data/retrofit/ApiSevice.kt
@@ -2,6 +2,8 @@ package com.example.githubuser.data.retrofit
 
 import com.example.githubuser.data.response.GithubUserDetailResponse
 import com.example.githubuser.data.response.GithubUsersResponse
+import com.example.githubuser.data.response.UserFollowersResponse
+import com.example.githubuser.data.response.UserFollowingResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -16,5 +18,13 @@ interface ApiSevice {
     // Method untuk mengambil detail data user
     @GET("users/{username}")
     fun getUserDetail(@Path("username") username: String): Call<GithubUserDetailResponse>
+
+    // Method untuk mengambil list data followers
+    @GET("users/{username}")
+    fun getUserFollowers(@Path("username") username: String): Call<UserFollowersResponse>
+
+    // Method untuk mengambil list data following
+    @GET("users/{username}")
+    fun getUserFollowing(@Path("username") username: String): Call<UserFollowingResponse>
 
 }
