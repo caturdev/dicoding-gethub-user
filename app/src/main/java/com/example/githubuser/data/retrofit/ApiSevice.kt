5a691daf5a554baa5ebacecf3cc750910836b7e9
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-01-12 14:50:35 +0700

    update api service

--- app/src/main/java/com/example/githubuser/data/retrofit/ApiSevice.kt
+++ app/src/main/java/com/example/githubuser/data/retrofit/ApiSevice.kt
@@ -1,11 +1,20 @@
 package com.example.githubuser.data.retrofit
 
+import com.example.githubuser.data.response.GithubUserDetailResponse
 import com.example.githubuser.data.response.GithubUsersResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiSevice {
+
+    // Method untuk mengambil data user berdasarkan querystring (username)
     @GET("search/users")
     fun getUser(@Query("q") name: String): Call<GithubUsersResponse>
+
+    // Method untuk mengambil detail data user
+    @GET("users/{username}")
+    fun getUserDetail(@Path("username") username: String): Call<GithubUserDetailResponse>
+
 }
