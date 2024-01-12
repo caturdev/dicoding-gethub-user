711459844c2a784369674839687b4f0c0fc2d1e5
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-01-12 09:39:21 +0700

    fix api service

--- app/src/main/java/com/example/githubuser/data/retrofit/ApiSevice.kt
+++ app/src/main/java/com/example/githubuser/data/retrofit/ApiSevice.kt
@@ -3,11 +3,9 @@ package com.example.githubuser.data.retrofit
 import com.example.githubuser.data.response.GithubUsersResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiSevice {
-    @GET("search/{name}")
-    fun getUser(
-        @Path("name") name: String
-    ): Call<GithubUsersResponse>
+    @GET("search/users")
+    fun getUser(@Query("q") name: String): Call<GithubUsersResponse>
 }
