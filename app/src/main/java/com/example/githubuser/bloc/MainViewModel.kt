6c592510d514e1780ef7ef2f9ce97198a120f3aa
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-01-12 10:39:21 +0700

    move function to main view model

--- app/src/main/java/com/example/githubuser/bloc/MainViewModel.kt
+++ app/src/main/java/com/example/githubuser/bloc/MainViewModel.kt
@@ -1,10 +1,70 @@
 package com.example.githubuser.bloc
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.githubuser.data.response.GithubUsersResponse
+import com.example.githubuser.data.response.ItemsItem
+import com.example.githubuser.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
 
-    
+    companion object {
+        private const val TAG = "MainViewModel"
+        private const val USER_ID = "sidiqpermana"
+    }
+
+    private val _users = MutableLiveData<List<ItemsItem>>()
+    val users: LiveData<List<ItemsItem>> = _users
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        getUsers()
+    }
+
+    private fun getUsers() {
+        // menampilkan loading indicator
+        _isLoading.value = true
+
+        val client = ApiConfig.getApiService().getUser(USER_ID)
+        client.enqueue(object : Callback<GithubUsersResponse> {
+
+            // response handler untuk response
+            override fun onResponse(
+                call: Call<GithubUsersResponse>,
+                response: Response<GithubUsersResponse>
+            ) {
+
+                // menyembunyikan loading indicator
+                _isLoading.value = false
+                Log.e(MainViewModel.TAG, "Res: $response")
+
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _users.value = responseBody.items
+                    }
+                } else {
+                    Log.e(MainViewModel.TAG, "Error: ${response.message()}")
+                }
+
+            }
+
+            // response handler saat terjadi gagal koneksi
+            override fun onFailure(call: Call<GithubUsersResponse>, t: Throwable) {
+
+                // menyembunyikan loading indicator
+                _isLoading.value = false
+
+            }
+
+        })
+    }
+
 }
--- app/src/main/java/com/example/githubuser/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/ui/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.airbnb.lottie.LottieAnimationView
 import com.example.githubuser.adapter.GithubUserListAdapter
+import com.example.githubuser.bloc.MainViewModel
 import com.example.githubuser.data.response.GithubUsersResponse
 import com.example.githubuser.data.response.ItemsItem
 import com.example.githubuser.data.retrofit.ApiConfig
@@ -35,13 +37,30 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        val mainViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        )[MainViewModel::class.java]
+
+        // -------------------------------
+        // Block untuk menangani list view
+        // -------------------------------
         val layoutManager = LinearLayoutManager(this)
         binding.rvUser.layoutManager = layoutManager
+
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.rvUser.addItemDecoration(itemDecoration)
 
-        getUsers()
+        mainViewModel.users.observe(this) { user -> setUserListData(user) }
 
+        // -------------------------------
+        // Block untuk menangani loading indicator
+        // -------------------------------
+        mainViewModel.isLoading.observe(this) { isLoading -> showLoading(isLoading) }
+
+        // -------------------------------
+        // Block untuk lottie animation
+        // -------------------------------
         val githubLoading = binding.githubLoading
         githubLoading.setAnimationFromUrl("https://lottie.host/f4aa2a91-160f-40bf-927a-85ca4d9f1074/HesvD4FI65.json")
     }
@@ -50,45 +69,6 @@ class MainActivity : AppCompatActivity() {
         val adapter = GithubUserListAdapter()
         adapter.submitList(user)
         binding.rvUser.adapter = adapter
-    }
-
-    private fun getUsers() {
-        // menampilkan loading indicator
-        showLoading(true)
-
-        val client = ApiConfig.getApiService().getUser(USER_ID)
-        client.enqueue(object : Callback<GithubUsersResponse> {
-
-            // response handler untuk response
-            override fun onResponse(
-                call: Call<GithubUsersResponse>,
-                response: Response<GithubUsersResponse>
-            ) {
-
-                // menyembunyikan loading indicator
-                showLoading(false)
-                Log.e(TAG, "Res: $response")
-
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        setUserListData(responseBody.items)
-                    }
-                } else {
-                    Log.e(TAG, "Error: ${response.message()}")
-                }
-
-            }
-
-            // response handler saat terjadi gagal koneksi
-            override fun onFailure(call: Call<GithubUsersResponse>, t: Throwable) {
-
-                // menyembunyikan loading indicator
-                showLoading(false)
-
-            }
-
-        })
     }
 
     // function untuk menampilkan atau menyembunyikan loading indicator
