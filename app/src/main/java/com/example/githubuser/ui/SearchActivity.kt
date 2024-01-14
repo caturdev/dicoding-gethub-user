f7e1868a2f12d52058ac4f352a4b5bff77cd7c7a
Catur Wicaksono <open.network.project@gmail.com>
2024-01-14 12:38:44 +0700

    add serarch activity

---
+++ app/src/main/java/com/example/githubuser/ui/SearchActivity.kt
@@ -0,0 +1,39 @@
+package com.example.githubuser.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.githubuser.databinding.ActivitySearchBinding
+
+class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+
+    companion object {
+        const val EXTRA_SELECTED_VALUE = "extra_selected_value"
+        const val RESULT_CODE = 110
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            // handler searchbar
+            searchView.editText.setOnEditorActionListener { textView, actionId, event ->
+                searchBar.setText(searchView.text)
+                searchView.hide()
+
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_SELECTED_VALUE, searchView.text.toString())
+                setResult(RESULT_CODE, resultIntent)
+                finish()
+
+                false
+            }
+        }
+    }
+}
